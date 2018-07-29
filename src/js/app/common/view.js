d2fa8d937e7cf95cce3ff103d60838e9eef76e23
Jesús Roldán <jesus.roldan@gmail.com>
2018-07-29 22:46:14 +0200

    Repositioning

--- src/js/app/common/view.js
+++ src/js/app/common/view.js
@@ -1,33 +1,79 @@
 import log from "core/logger";
+import '@nasaworldwind/worldwind';
 
 export default class {
     constructor() {
         var wwd = new WorldWind.WorldWindow("canvasMap");
-    
         WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
-    
-        // Add some image layers to the WorldWindow's globe.
+
+        // BMNGLayer Blue Marbe layer: aka The Globe
+        var configLayers = {
+            showBMNGLayer: true,
+            showBMNGLandsatLayer: false,
+            showBingAerialLayer: false,
+            showBingAerialWithLabelsLayer: false,
+            showBingRoadsLayer: false,
+            showOpenStreetMapImageLayer: false,
+            showAtmosphereLayer: true,
+            showCompassLayer: true,
+            showCoordinatesDisplayLayer: false,
+            showViewControlsLayer: false
+        };
+
         var layers = [
-            // Imagery layers.
-            {layer: new WorldWind.BMNGLayer(), enabled: true},
-            {layer: new WorldWind.BMNGLandsatLayer(), enabled: false},
-            {layer: new WorldWind.BingAerialLayer(null), enabled: false},
-            {layer: new WorldWind.BingAerialWithLabelsLayer(null), enabled: true},
-            {layer: new WorldWind.BingRoadsLayer(null), enabled: false},
-            {layer: new WorldWind.OpenStreetMapImageLayer(null), enabled: false},
+            {
+                layer: new WorldWind.BMNGLayer(),
+                enabled: configLayers.showBMNGLayer
+            },
+            {
+                layer: new WorldWind.BMNGLandsatLayer(),
+                enabled: configLayers.showBMNGLandsatLayer
+            },
+            {
+                layer: new WorldWind.BingAerialLayer(null),
+                enabled: configLayers.showBingAerialLayer
+            },
+            {
+                layer: new WorldWind.BingAerialWithLabelsLayer(null),
+                enabled: configLayers.showBingAerialWithLabelsLayer
+            },
+            {
+                layer: new WorldWind.BingRoadsLayer(null),
+                enabled: configLayers.showBingRoadsLayer
+            },
+            {
+                layer: new WorldWind.OpenStreetMapImageLayer(null),
+                enabled: configLayers.showOpenStreetMapImageLayer
+            },
             // Add atmosphere layer on top of all base layers.
-            {layer: new WorldWind.AtmosphereLayer(), enabled: true},
+            {
+                layer: new WorldWind.AtmosphereLayer(),
+                enabled: configLayers.showAtmosphereLayer
+            },
             // WorldWindow UI layers.
-            {layer: new WorldWind.CompassLayer(), enabled: true},
-            {layer: new WorldWind.CoordinatesDisplayLayer(wwd), enabled: true},
-            {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true}
+            {
+                layer: new WorldWind.CompassLayer(),
+                enabled: configLayers.showCompassLayer
+            },
+            {
+                layer: new WorldWind.CoordinatesDisplayLayer(wwd),
+                enabled: configLayers.showCoordinatesDisplayLayer
+            },
+            {
+                layer: new WorldWind.ViewControlsLayer(wwd),
+                enabled: configLayers.showViewControlsLayer
+            }
         ];
-    
+
         for (var l = 0; l < layers.length; l++) {
             layers[l].layer.enabled = layers[l].enabled;
             wwd.addLayer(layers[l].layer);
         }
+
+        // Finland position
+        wwd.navigator.lookAtLocation.latitude = 61.9241;
+        wwd.navigator.lookAtLocation.longitude = 25.7482;
+        wwd.navigator.range = 2500000;
+        wwd.redraw();
     }
 }
-
-
